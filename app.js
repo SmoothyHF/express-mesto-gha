af3bd012d57a8db620cc2f3f861162066c509836
Armand Allakhverdyan <armand.all@mail.ru>
2023-12-21 00:53:09 +0700

    mesto-back

--- app.js
+++ app.js
@@ -7,15 +7,27 @@ const auth = require('./middlewares/auth');
 const { login, createUser } = require('./controllers/users');
 const errorHandler = require('./middlewares/error-handler');
 const NotFoundError = require('./errors/notFound-error');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
+const {
+  PORT = 3000,
+  MONGO_URL = 'mongodb://127.0.0.1:27017/mestodb',
+} = process.env;
+
+mongoose.connect(`${MONGO_URL}`, {
   useNewUrlParser: true,
 }).then();
 
+// mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
+//   useNewUrlParser: true,
+// }).then();
+
 const app = express();
-const port = 3000;
+// const port = 3000;
 
 app.use(express.json());
+
+app.use(requestLogger);
 
 app.post('/signup', celebrate({
   body: Joi.object().keys({
@@ -40,8 +52,10 @@ app.use(appRouter);
 
 app.use((req, res, next) => next(new NotFoundError('Указан неверный адрес')));
 
+app.use(errorLogger);
+
 app.use(errors());
 
 app.use(errorHandler);
 
-app.listen(port);
+app.listen(PORT);
